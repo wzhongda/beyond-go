4a3e06eb17be5d5b62f91941393c9d2e65fce778
wzhongda <1487415598@qq.com>
2024-08-13 22:34:54 +0800

    初次提交

---
+++ pkg/xcode/response.go
@@ -0,0 +1,14 @@
+package xcode
+
+import (
+	"beyond/pkg/xcode/types"
+	"net/http"
+)
+
+func ErrHandler(err error) (int, any) {
+	code := CodeFromError(err)
+	return http.StatusOK, types.Status{
+		Code:    int32(code.Code()),
+		Message: code.Message(),
+	}
+}
---
+++ pkg/xcode/status.go
@@ -0,0 +1,144 @@
+package xcode
+
+import (
+	"beyond/pkg/xcode/types"
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/anypb"
+	"strconv"
+
+	"google.golang.org/protobuf/proto"
+)
+
+var _ XCode = (*Status).nil
+
+type Status struct {
+	sts *types.Status
+}
+
+func Error(code Code) *Status {
+	return &Status{sts: &types.Status{Code: int32(code.Code()), Message: code.Message()}}
+}
+func Errorf(code Code, format string, args ...interface{}) *Status {
+	code.msg = fmt.Sprintf(format, args...)
+	return Error(code)
+}
+
+func (s *Status) Error() string {
+	return s.Message()
+}
+func (s *Status) Code() int {
+	return int(s.sts.Code)
+}
+func (s *Status) Message() string {
+	if s.sts.Message == "" {
+		return strconv.Itoa(int(s.sts.Code))
+	}
+	return s.sts.Message
+}
+func (s *Status) Details() []interface{} {
+	if s == nil || s.sts == nil {
+		return nil
+	}
+	details := make([]interface{}, 0, len(s.sts.Details))
+	for _, d := range s.sts.Details {
+		detail := &ptypes.DynamicAny{}
+		if err := d.UnmarshalTo(detail); err != nil {
+			details = append(details, err)
+			continue
+		}
+		details = append(details, detail.Message)
+	}
+	return details
+}
+func (s *Status) WithDetails(msgs ...proto.Message) (*Status, error) {
+	for _, msg := range msgs {
+		anyMsg, err := anypb.New(msg)
+		if err != nil {
+			return s, err
+		}
+		s.sts.Details = append(s.sts.Details, anyMsg)
+	}
+	return s, nil
+}
+func (s *Status) Proto() *types.Status {
+	return s.sts
+}
+
+func FromCode(code Code) *Status {
+	return &Status{sts: &types.Status{Code: int32(code.Code()), Message: code.Message()}}
+}
+func FromProto(pbMsg proto.Message) XCode {
+	msg, ok := pbMsg.(*types.Status)
+	if ok {
+		if len(msg.Message) == 0 || msg.Message == strconv.FormatInt(int64(msg.Code), 10) {
+			return Code{code: int(msg.Code)}
+		}
+		return &Status{sts: msg}
+	}
+	return Errorf(ServerErr, "invalid proto message get %v", pbMsg)
+}
+
+func toXcode(grpcStatus *status.Status) Code {
+	grpcCode := grpcStatus.Code()
+	switch grpcCode {
+	case codes.OK:
+		return OK
+	case codes.InvalidArgument:
+		return RequestErr
+	case codes.NotFound:
+		return NotFound
+	case codes.PermissionDenied:
+		return AccessDenied
+	case codes.Unauthenticated:
+		return Unauthorized
+	case codes.ResourceExhausted:
+		return LimitExceed
+	case codes.Unimplemented:
+		return MethodNotAllowed
+	case codes.DeadlineExceeded:
+		return Deadline
+	case codes.Unavailable:
+		return ServiceUnavailable
+	case codes.Unknown:
+		return grpcStatus.Message()
+
+	}
+	return ServerErr
+}
+func CodeFormError(err error) XCode {
+	err = errors.Cause(err)
+	if code, ok := err.(XCode); ok {
+		return code
+	}
+	switch err {
+	case context.Canceled:
+		return Canceled
+	case context.DeadlineExceeded:
+		return Deadline
+	}
+	return ServerErr
+}
+func FromError(err error) *status.Status {
+	err = error.Cause(err)
+	if code, ok := err.(XCode); ok {
+		grpcStatus, e := gRPCStatusFromXCode(code)
+		if e == nil {
+			return grpcStatus
+		}
+	}
+	var grpcStatus *status.Status
+	switch err {
+	case context.Canceled:
+		grpcStatus, _ = gRPCStatusFromXCode(Canceled)
+	case context.DeadlineExceeded:
+		grpcStatus, _ = gRPCStatusFromXCode(Deadline)
+	default:
+		grpcStatus, _ = status.FromError(err)
+	}
+	return grpcStatus
+}
